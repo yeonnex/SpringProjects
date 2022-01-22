5f7ef89ef52383bdc4c089c77e68969a9eec1370
yeonnex <82166132+yeonnex@users.noreply.github.com>
2022-01-22 10:40:34 +0900

    ContentCachingRequestWrapper 로 캐싱하여 버퍼(user request)를 한번 읽었어도 또 읽을 수 있게하여 에러 해결

--- filter/src/main/java/com/example/filter/filter/GlobalFilter.java
+++ filter/src/main/java/com/example/filter/filter/GlobalFilter.java
@@ -2,6 +2,8 @@ package com.example.filter.filter;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
@@ -14,18 +16,21 @@ public class GlobalFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         // 전처리
-        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
-        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
-
-        String url = httpServletRequest.getRequestURI();
-        BufferedReader br = httpServletRequest.getReader();
-
-        br.lines().forEach(line ->{
-            log.info("url : {}, line : {}", url, line);
-        });
+        ContentCachingRequestWrapper httpServletRequest = new ContentCachingRequestWrapper((HttpServletRequest)request);
+        ContentCachingResponseWrapper httpServletResponse = new ContentCachingResponseWrapper((HttpServletResponse)response);
 
         chain.doFilter(httpServletRequest, httpServletResponse);
 
+        String url = httpServletRequest.getRequestURI();
         // 후처리
+        // req
+        String reqContent = new String(httpServletRequest.getContentAsByteArray());
+        log.info("request url: {}, request body: {}", url, reqContent);
+
+        String resContent = new String(httpServletResponse.getContentAsByteArray());
+        int httpStatus = httpServletResponse.getStatus();
+
+        httpServletResponse.copyBodyToResponse();
+        log.info("response status : {}, response body : {}", httpStatus, resContent);
     }
 }
