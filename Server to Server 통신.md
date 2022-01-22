265d030f1b31c3aa993312656391ede97c5412ec
yeonnex <82166132+yeonnex@users.noreply.github.com>
2022-01-22 12:54:44 +0900

    http://localhost:9090/api/server/hello 로 요청 보내는 client

---
+++ Server to Server 통신.md
@@ -0,0 +1,7 @@
+지금까지, 우리는 서버의 입장이었음. 그래서 브라우저나 talend API, 또는 핸드폰에서 우리 서버로 클라이언트가 접근하는 상황만을 공부했었다.
+
+> 이번에는, 우리의 서버가 클라이언트가 되어서 다른 서버에 요청을 해보자!
+
+스프링에서는 RestTemplate을 주로 쭉 많이 써왔다. RestTemplate 를 사용해서 서버에 연결해보자!
+
+![image-20220122121532915](https://raw.githubusercontent.com/yeonnex/image-server/main/img/image-20220122121532915.png)
---
+++ server2server/client/src/main/java/com/example/client/service/RestTemplateService.java
@@ -0,0 +1,29 @@
+package com.example.client.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@Service
+public class RestTemplateService {
+    public String hello(){
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9090")
+                .path("/api/server/hello")
+                .encode()
+                .build()
+                .toUri();
+
+        System.out.println(uri);
+
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(uri, String.class); // 서버로 요청하는 순간!
+
+        return result;
+
+
+    }
+
+}
