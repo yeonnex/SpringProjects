9bafae60b738292ce51360dc4258366c588c2276
yeonnex <82166132+yeonnex@users.noreply.github.com>
2022-01-27 11:16:43 +0900

    JPA - create Test

---
+++ study/src/main/java/com/example/study/model/entity/User.java
@@ -0,0 +1,31 @@
+package com.example.study.model.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data // 롬복! 생성자와 겟셋 메서드 자동생성~
+@Entity // ==table
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String account;
+    private String email;
+    private String phoneNumber;
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime updatedAt;
+    private String updatedBy;
+}
---
+++ study/src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -0,0 +1,38 @@
+package com.example.study.repository;
+
+import com.example.study.StudyApplication;
+import com.example.study.model.entity.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.example.study.StudyApplicationTests;
+import java.time.LocalDateTime;
+
+public class UserRepositoryTest extends StudyApplicationTests {
+
+    // Dependency Injection (DI)
+    @Autowired UserRepository userRepository;
+
+    @Test
+    public void create(){
+        // String sql = "insert into user (%s, %s, %d) values (account, email, age);
+        User user = new User();
+        // user.setId(); DB에서 Auto Increment 될 것이기 때문
+        user.setAccount("TestUser03");
+        user.setEmail("TestUser03@gmail.com");
+        user.setPhoneNumber("010-333-3333");
+        user.setCreatedAt(LocalDateTime.now());
+        user.setCreatedBy("admin");
+
+        User newUser = userRepository.save(user);
+        System.out.println("new User: " + newUser);
+    }
+    public void read(){
+
+    }
+    public void update(){
+
+    }
+    public void delete(){
+
+    }
+}
