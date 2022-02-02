208912057a5c52671118b2a6777f6fa0d88945d0
yeonnex <82166132+yeonnex@users.noreply.github.com>
2022-02-02 18:42:48 +0900

    OrderGroup update API 정상작동

--- study/src/main/java/com/example/study/service/OrderGroupApiLogicService.java
+++ study/src/main/java/com/example/study/service/OrderGroupApiLogicService.java
@@ -47,7 +47,26 @@ public class OrderGroupApiLogicService implements CrudInterface<OrderGroupApiReq
 
     @Override
     public Header<OrderGroupApiResponse> update(Header<OrderGroupApiRequest> request) {
-        return null;
+        OrderGroupApiRequest body = request.getData();
+        return orderGroupRepository.findById(body.getId())
+                .map(orderGroup -> {
+                    orderGroup
+                            .setStatus(body.getStatus())
+                            .setOrderType(body.getOrderType())
+                            .setRevName(body.getRevName())
+                            .setRevName(body.getRevName())
+                            .setPaymentType(body.getPaymentType())
+                            .setOrderAt(body.getOrderAt())
+                            .setArrivalDate(body.getArrivalDate())
+                            .setTotalPrice(body.getTotalPrice())
+                            .setTotalQuantity(body.getTotalQuantity())
+                            .setUser(userRepository.getOne(body.getUserId()))
+                            ;
+                    return orderGroup;
+                })
+                .map(changeOrderGroup -> orderGroupRepository.save(changeOrderGroup))
+                .map(this::response)
+                .orElseGet(() -> Header.ERROR("데이터 없음"));
     }
 
     @Override
