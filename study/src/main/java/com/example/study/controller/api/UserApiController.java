67b319cda249d6c96a0967fa71054c26864467f8
yeonnex <82166132+yeonnex@users.noreply.github.com>
2022-02-02 19:21:37 +0900

    Crud 추상클래스 파일로 만듦 - CrudController.java (리팩토링)

--- study/src/main/java/com/example/study/controller/api/UserApiController.java
+++ study/src/main/java/com/example/study/controller/api/UserApiController.java
@@ -1,6 +1,7 @@
 package com.example.study.controller.api;
 
 
+import com.example.study.controller.CrudController;
 import com.example.study.ifs.CrudInterface;
 import com.example.study.model.network.Header;
 import com.example.study.model.network.request.UserApiRequest;
@@ -10,35 +11,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
+
 @Slf4j
 @RestController
 @RequestMapping("/api/user")
-public class UserApiController implements CrudInterface<UserApiRequest, UserApiResponse> {
+public class UserApiController extends CrudController<UserApiRequest, UserApiResponse> {
     @Autowired
     private UserApiLogicService userApiLogicService;
-    @Override
-    @PostMapping("") // /api/user
-    public Header<UserApiResponse> create(@RequestBody Header<UserApiRequest> request) {
-        log.info("{}", request);
-        return userApiLogicService.create(request);
+    @PostConstruct
+    public void init(){
+        this.baseService = userApiLogicService;
     }
 
-    @Override
-    @GetMapping("{id}") // /api/user/{id}
-    public Header<UserApiResponse> read(@PathVariable Long id) {
-        log.info("read id : {}", id);
-        return userApiLogicService.read(id);
-    }
-
-    @Override
-    @PutMapping("") // /api/user
-    public Header<UserApiResponse> update(@RequestBody Header<UserApiRequest> userApiRequest) {
-        return userApiLogicService.update(userApiRequest);
-    }
-
-    @Override
-    @DeleteMapping("{id}") // /api/user/{id}
-    public Header<UserApiResponse> delete(@PathVariable Long id) {
-        return userApiLogicService.delete(id);
-    }
 }
