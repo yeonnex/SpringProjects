aef05f493e88a924d4ecffe23a5f750ce7fb0b7d
yeonnex <82166132+yeonnex@users.noreply.github.com>
2022-02-03 12:58:25 +0900

    blog 프로젝트 시작

---
+++ blog/src/test/java/com/yeonnex/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeonnex.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
