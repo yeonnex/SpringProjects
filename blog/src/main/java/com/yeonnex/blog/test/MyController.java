4a3dbc18d3484275aeae069c2f18cd103eb8185a
yeonnex <82166132+yeonnex@users.noreply.github.com>
2022-02-04 14:44:49 +0900

    유저 select all, 페이징 처리 - @PageableDefault - Pageable

--- blog/src/main/java/com/yeonnex/blog/test/MyController.java
+++ blog/src/main/java/com/yeonnex/blog/test/MyController.java
@@ -3,14 +3,20 @@ package com.yeonnex.blog.test;
 import com.yeonnex.blog.model.User;
 import com.yeonnex.blog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.function.Supplier;
 
+// html 파일이 아니라 data 를 리턴해주는 controller = RestController
 @RestController
 public class MyController {
     @Autowired
@@ -26,6 +32,18 @@ public class MyController {
         return Optional.ofNullable(userRepository.findById(id).orElseThrow(()->{
             return new IllegalArgumentException("해당 사용자는 없음");
         }));
+    }
+
+    // 한 페이지당 2건의 데이터를 리턴받을 에정정
+   @GetMapping("/temp/user")
+    public Page<User> pageList(@PageableDefault(size=2, sort = "id", direction = Sort.Direction.DESC)Pageable pageable){
+        return userRepository.findAll(pageable);
+        // return userRepository.findAll(pageable).getContent();
+    }
+
+    @GetMapping("/temp/users")
+    public List<User> allUser(){
+        return userRepository.findAll();
     }
 
     @GetMapping("/temp/home")
