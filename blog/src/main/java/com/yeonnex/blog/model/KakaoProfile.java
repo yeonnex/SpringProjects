95cb15941bf4bae81565237eab5846ffe76dfb49
yeonnex <82166132+yeonnex@users.noreply.github.com>
2022-02-17 17:32:59 +0900

    [리팩토링] KakaoProfile 모델 클래스 -> 내부 클래스로 깔끔하게

--- blog/src/main/java/com/yeonnex/blog/model/KakaoProfile.java
+++ blog/src/main/java/com/yeonnex/blog/model/KakaoProfile.java
@@ -9,34 +9,39 @@ public class KakaoProfile {
     public Properties properties;
     public KakaoAccount kakaoAccount;
 
+    class KakaoAccount {
+
+        public Boolean profileNicknameNeedsAgreement;
+        public Boolean profileImageNeedsAgreement;
+        public Profile profile;
+        public Boolean hasEmail;
+        public Boolean emailNeedsAgreement;
+        public Boolean isEmailValid;
+        public Boolean isEmailVerified;
+        public String email;
+
+        class Profile {
+
+            public String nickname;
+            public String thumbnailImageUrl;
+            public String profileImageUrl;
+            public Boolean isDefaultImage;
+
+        }
+
+    }
+
+    class Properties {
+
+        public String nickname;
+        public String profileImage;
+        public String thumbnailImage;
+
+    }
+
 }
 
-class KakaoAccount {
-
-    public Boolean profileNicknameNeedsAgreement;
-    public Boolean profileImageNeedsAgreement;
-    public Profile profile;
-    public Boolean hasEmail;
-    public Boolean emailNeedsAgreement;
-    public Boolean isEmailValid;
-    public Boolean isEmailVerified;
-    public String email;
-
-}
 
 
-class Profile {
 
-    public String nickname;
-    public String thumbnailImageUrl;
-    public String profileImageUrl;
-    public Boolean isDefaultImage;
 
-}
-class Properties {
-
-    public String nickname;
-    public String profileImage;
-    public String thumbnailImage;
-
-}
