896d6a66ec6de8a589f860b9a082b8adcf917b14
yeonnex <82166132+yeonnex@users.noreply.github.com>
2022-02-22 19:38:30 +0900

    패스워드 암호화하여 회원가입, 회원가입 정상 진행시 로그인폼으로 리다이렉트 - BCryptPasswordEncoder 를 Bean 으로 등록 - return "redirect:/loginForm"

--- securitybasic/src/main/java/com/example/securitybasic/config/SecurityConfig.java
+++ securitybasic/src/main/java/com/example/securitybasic/config/SecurityConfig.java
@@ -1,14 +1,23 @@
 package com.example.securitybasic.config;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity // 얘를 활성화하면 스프링 시큐리티 필터가 스프링 필터체인에 등록이 됩니다.
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // 이렇게 Bean 으로 등록하면 해당 메서드의 리턴되는 오브젝트를 IoC 로 등록해준다다
+   @Bean
+    public BCryptPasswordEncoder encodePwd(){
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
--- securitybasic/src/main/java/com/example/securitybasic/controller/MainController.java
+++ securitybasic/src/main/java/com/example/securitybasic/controller/MainController.java
@@ -3,6 +3,7 @@ package com.example.securitybasic.controller;
 import com.example.securitybasic.model.User;
 import com.example.securitybasic.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,10 @@ public class MainController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder encoder;
+
     // localhost:8080/
     // localhost:8080
     @GetMapping({"", "/"})
@@ -48,10 +53,12 @@ public class MainController {
     }
 
     @PostMapping("/join")
-    public @ResponseBody String join(User user){
+    public String join(User user){
         System.out.println(user);
         user.setRole("ROLE_USER");
+        String pwd = user.getPassword();
+        user.setPassword(encoder.encode(pwd));
         userRepository.save(user); // 회원가입 잘 됨. 비밀번호 : 1234 => 시큐리티로 로그인을 할 수 없음. 이유는 패스워드가 암호화가 안되었기 때문
-        return "join";
+        return "redirect:/loginForm";
     }
 }
