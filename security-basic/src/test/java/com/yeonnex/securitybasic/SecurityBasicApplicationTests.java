7afbe915b72fd772d1791a18fb1dbb692a6ef38e
yeonnex <82166132+yeonnex@users.noreply.github.com>
2022-02-22 16:47:43 +0900

    되돌리기

---
+++ security-basic/src/test/java/com/yeonnex/securitybasic/SecurityBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeonnex.securitybasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
