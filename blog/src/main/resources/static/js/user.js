7b29c5c099899618014bcd0e944d3ba4d718923b
yeonnex <82166132+yeonnex@users.noreply.github.com>
2022-02-22 13:04:06 +0900

    [Fix] 회원 중복 아이디 가입 문제 해결

--- blog/src/main/resources/static/js/user.js
+++ blog/src/main/resources/static/js/user.js
@@ -27,9 +27,12 @@ let index = {
            contentType: "application/json; charset=utf-8", // body 데이터가 어떤 타입인지(MIME)
            dataType: "json" // 요청을 서버로 해서 응답이 왔을 때 기본적으로 모든 것이 문자열 (생긴게 json 이라면) => javascript 오브젝트로 바꿔줌
         }).done(function (resp) {
-            // 정상이면 여기 실행
-           alert("회원가입이 완료되었습니다");
-           location.href = "/";
+            if(resp.status === 500){
+                alert("회원가입에 실패하였습니다");
+            }else{
+               alert("회원가입이 완료되었습니다");
+               location.href = "/";
+            }
         }).fail(function (error){
             // 실패면 여기 실행
             alert("회원가입 실패!");
