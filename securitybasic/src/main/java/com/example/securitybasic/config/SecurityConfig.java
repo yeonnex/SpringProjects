82b5dc3c67eebff4f38d30eead469eddfeb3e617
yeonnex <82166132+yeonnex@users.noreply.github.com>
2022-02-23 13:24:25 +0900

    구글 회원 프로필 정보 받아오기 - loadUser 에서 구글로부터 받은 userRequest 데이터에 대한 후처리 - userRequest.getAccessToken().getTokenValue() - super.loadUser(userRequest).getAttributes() - Attributes: {sub=104169802434657415471, name=장서연, given_name=서연, family_name=장, picture=https://lh3.googleusercontent.com/a-/AOh..., email=yeonnex@gmail.com, email_verified=true, locale=ko}

--- securitybasic/src/main/java/com/example/securitybasic/config/SecurityConfig.java
+++ securitybasic/src/main/java/com/example/securitybasic/config/SecurityConfig.java
@@ -1,6 +1,13 @@
 package com.example.securitybasic.config;
 
+/**
+ * 1. 코드받기(인증), 2. 액세스토큰(권한)
+ * 3. 사용자프로필 정보를 가져오고, 4-1. 그 정보(이메일, 전화번호, 이름, 아이디)를 토대로 회원가입을 자동으로 진행시키기도 함
+ * 4-2. 정보가 부족한경우, ex)쇼핑몰 -> (집주소), 백화점몰 -> (vip 등급, 일반등급)
+ */
 
+import com.example.securitybasic.config.oauth.PrincipalOauth2UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -13,6 +20,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableWebSecurity // 얘를 활성화하면 스프링 시큐리티 필터가 스프링 필터체인에 등록이 됩니다.
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true) // secured 어노테이션 활성화, preAuthorize, postAuthorize 어노테이션 활성화
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private PrincipalOauth2UserService principalOauth2UserService;
 
     // 이렇게 Bean 으로 등록하면 해당 메서드의 리턴되는 오브젝트를 IoC 로 등록해준다다
     @Bean
@@ -38,6 +48,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .defaultSuccessUrl("/") // 로그인 성공시 메인 페이지로 가게하겠다.
                 .and()
                 .oauth2Login()
-                .loginPage("/loginForm"); // 구글 로그인이 완료된 후의 후처리가필요함
+                .loginPage("/loginForm") // 구글 로그인이 완료된 후의 후처리가필요함
+                .userInfoEndpoint()
+                .userService(principalOauth2UserService);
+        /**
+         * Tip. 구글 로그인이 완료되면, 코드를 받는 것이 아니라,
+         * 액세스토큰 + 사용자 프로필 정보를 한방에 받는다.
+         * 그래서 oauth client 라이브러리가 매우 편리하다는 것!
+         */
     }
 }
