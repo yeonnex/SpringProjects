5bb2be77a64b34b5da43c0b2974a3fd733b88de2
yeonnex <82166132+yeonnex@users.noreply.github.com>
2022-02-25 18:19:35 +0900

    CorsConfig.java 에서 Cors 설정한 것 필터로 등록 - addFilter(corsFilter) - 이 서버는 stateless 서버 - 이 서버는 크로스 오리진 정책에서 벗어남. 모든 요청 허용 - 이 서버는 폼 로그인도 쓰지 않을것임

--- jwt/src/main/java/com/example/jwt/config/SecurityConfig.java
+++ jwt/src/main/java/com/example/jwt/config/SecurityConfig.java
@@ -1,15 +1,26 @@
 package com.example.jwt.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.filter.CorsFilter;
 
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CorsFilter corsFilter; // 모든 요청이 이 필터를 타게 되어있음. 이게 걸려있는 한, cors 요청이 와도 다 허용됨!
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션을 사용하지 않겠다는 뜻. 즉 stateless 서버로 만들겠따!
                 .and()
+                .addFilter(corsFilter) // @CrossOrigin(인증 X), 시큐리티 필터에 등록 인증(O)
                 .formLogin().disable() // 왜냐, jwt 서버니까 id,비번 폼 로그인을 하지 않음!
                 .httpBasic().disable()
                 .authorizeRequests()
